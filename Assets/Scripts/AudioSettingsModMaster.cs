894b3dc28cec7830c2c28b239c92971a1e4af5cb
Matteo1903 <marinimatteo99@gmail.com>
2020-11-29 11:15:48 +0100

    Aggiunti script per la gestione dell'audio

---
+++ Assets/Scripts/AudioSettingsModMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AudioSettingsModMaster : MonoBehaviour
+{
+    public Slider s;
+    private float volume;
+    private SettingMaster sm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.FindGameObjectWithTag("SM").GetComponent<SettingMaster>();
+        s.value = sm.prefMaster;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Awake()
+    {
+
+
+    }
+}
