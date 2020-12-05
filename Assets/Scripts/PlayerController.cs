50b44b74af8c59c866122011871121b144c4a676
Berticus28 <federicobertinelli@live.it>
2020-12-05 17:43:19 +0100

    scritti script per la gestione del player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isRunning;
+    public GameObject RunningAudio;
+    public float speed;
+    public float jumpForce;
+    public GameObject standIm;
+    public GameObject crouchIm;
+    public Transform shield;
+    private float moveInput;
+    Rigidbody2D rb2d;
+    private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public Animator anim;
+    private enum State { idle, running, jumping,crouch}
+    private State state;
+    protected Joystick joystick;
+    public static bool isCrouched = false;
+    public Collider2D stand;
+    public Collider2D crouch;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        isCrouched = false;
+        joystick = FindObjectOfType<FixedJoystick>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateM();
+            RunningAudio.GetComponent<AudioSource>().mute=!isRunning;
+        anim.SetInteger("State", (int)state);
+    }
+    void FixedUpdate()
+    {
+        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = joystick.Horizontal;
+        if(!isCrouched)
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+       
+        
+    }
+   public void Jump()
+    {
+        if (isGrounded == true && !isCrouched)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+    
+    public void Crouch()
+    {
+        if (state != State.jumping)
+        {
+            if (isCrouched)
+            {
+                isCrouched = false;
+                stand.enabled = true;
+                crouch.enabled = false;
+                crouchIm.SetActive(true);
+                standIm.SetActive(false);
+                shield.Rotate(0f, 0f, -90f);
+                shield.position = new Vector3(shield.position.x, shield.position.y+0.6f, shield.position.z);
+
+            }
+            else
+            {
+                shield.Rotate(0f, 0f, 90f);
+                shield.position = new Vector3(shield.position.x, shield.position.y-0.6f, shield.position.z);
+                isCrouched = true;
+                stand.enabled = false;
+                crouch.enabled = true;
+                crouchIm.SetActive(false);
+                standIm.SetActive(true);
+
+            }
+        }
+    }
+    private void StateM()
+    {
+        if (!isGrounded)
+        {
+            state = State.jumping;
+            isRunning = false;
+        }
+        else if (isCrouched)
+        {
+            state = State.crouch;
+            isRunning = false;
+        }
+        else if (rb2d.velocity.x != 0)
+        {
+            state = State.running;
+            isRunning =true;
+        }
+        else 
+        {
+            state = State.idle;
+            isRunning = false;
+        }
+        
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerStat.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{  // Start is called before the first frame update
+    public GameObject DannoAudio;
+    public GameObject ShieldAudio;
+    public GameObject StellaAudio;
+    public Rigidbody2D rb2d;
+    public int health;
+    public float delayTime;
+    public bool isInvincible = false;
+    public bool test = false;
+    [SerializeField] private float invincibilityDurationSeconds;
+    public GameObject hp3;
+    public GameObject hp2;
+    public GameObject hp1;
+    public GameObject player;
+    public GameObject shield;
+    public GameObject InterfaceUI;
+    public GameObject LoseMenuUI;
+    public GameObject WinMenuUI;
+    public GameObject star1l;
+    public GameObject star2l;
+    public GameObject star3l;
+    public GameObject star4l;
+    public GameObject star5l;
+    public GameObject star6l;
+    public GameObject star1w;
+    public GameObject star2w;
+    public GameObject star3w;
+    public GameObject star4w;
+    public GameObject star5w;
+    public GameObject star6w;
+    public int starCount;
+    private GameMaster gm;
+    public List<string> collected = new List<string>();
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckpointPos.position+ new Vector3(0f,2f,0f);
+        if (GameObject.FindGameObjectWithTag("Respawn").transform.position + new Vector3(0f, 2f,0f)==transform.position)
+        {
+
+        }
+        else
+        {
+            starCount = gm.strCount;
+            collected = gm.collected;
+            foreach (string nom in collected)
+            {
+                GameObject.Find(nom).SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInvincible)
+        {
+            shield.SetActive(true);
+        }
+        if (!isInvincible)
+        {
+            shield.SetActive(false);
+        }
+        if (health==3)
+        {
+            hp3.SetActive(true);
+        }
+        if (health == 2)
+        {
+            hp3.SetActive(false);
+            hp2.SetActive(true);
+        }
+        if (health == 1)
+        {
+            hp2.SetActive(false);
+            hp1.SetActive(true);
+        }
+        if (health == 0)
+        {
+            hp1.SetActive(false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "EnemyBullet")
+        {
+            if (!isInvincible)
+            {
+                health -= 1;
+
+                if (health <= 0)
+                {
+                    StartCoroutine(Die());
+                }
+                DannoAudio.GetComponent<AudioSource>().Play();
+                StartCoroutine(BecomeTemporarilyInvincible());
+            }
+            else
+                ShieldAudio.GetComponent<AudioSource>().Play();
+        }
+        if (col.tag == "HP")
+        {
+            if (health < 3)
+            {
+                health++;
+                col.gameObject.SetActive(false);
+                collected.Add(col.gameObject.name);
+            }
+            else
+            {
+                StellaAudio.GetComponent<AudioSource>().Play();
+                starCount++;
+                col.gameObject.SetActive(false);
+                collected.Add(col.gameObject.name);
+            }
+        }
+        if (col.tag == "Star")
+        {
+            StellaAudio.GetComponent<AudioSource>().Play();
+            starCount += 1;
+            col.gameObject.SetActive(false);
+            collected.Add(col.gameObject.name);
+        }
+
+
+
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "abisso")
+        {
+            Debug.Log("L hai toccato!");
+            StartCoroutine(Die());
+
+        }
+
+        if (col.gameObject.tag == "End")
+        {
+            WinMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            InterfaceUI.SetActive(false);
+            if (starCount >= 1)
+            {
+                
+                star1w.SetActive(true);
+                if (starCount >= 2)
+                {
+                    star2w.SetActive(true);
+                    if (starCount >= 3)
+                    {
+                        star3w.SetActive(true);
+                        if (starCount >= 4)
+                        {
+                            star4w.SetActive(true);
+                            if (starCount >= 5)
+                            {
+                                star5w.SetActive(true);
+                                if (starCount >= 6)
+                                {
+                                    star6w.SetActive(true);
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }
+            
+        }
+    }
+    
+
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        Debug.Log("Player turned invincible!");
+        isInvincible = true;
+        yield return new WaitForSeconds(invincibilityDurationSeconds);
+
+        isInvincible = false;
+        Debug.Log("Player is no longer invincible!");
+    }
+
+
+    private IEnumerator Die()
+    {
+        if (test)
+        {
+            yield break;
+        }
+        test = true;
+        LoseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        InterfaceUI.SetActive(false);
+        if (starCount >= 1)
+        {
+            yield return new WaitForSecondsRealtime(delayTime);
+            star1l.SetActive(true);
+            if (starCount >= 2)
+            {
+                yield return new WaitForSecondsRealtime(delayTime);
+                star2l.SetActive(true);
+                if (starCount >= 3)
+                {
+                    yield return new WaitForSecondsRealtime(delayTime);
+                    star3l.SetActive(true);
+                    if (starCount >= 4)
+                    {
+                        yield return new WaitForSecondsRealtime(delayTime);
+                        star4l.SetActive(true);
+                        if (starCount >= 5)
+                        {
+                            yield return new WaitForSecondsRealtime(delayTime);
+                            star5l.SetActive(true);
+                            if (starCount >= 6)
+                            {
+                                yield return new WaitForSecondsRealtime(delayTime);
+                                star6l.SetActive(true);
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+        test = false;
+    }
+    public void Esplosione()
+    {
+        this.gameObject.GetComponent<AudioSource>().Play();
+    }
+   
+}
