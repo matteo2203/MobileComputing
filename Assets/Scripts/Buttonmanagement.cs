bd70da2213825032d5e954af117f7ac95a03b962
matteomaraziti <m.maraziti@gmail.com>
2020-12-10 12:21:12 +0100

    scritti script per la gestione dei bottoni

---
+++ Assets/Scripts/Buttonmanagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttonmanagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+        Destroy(GameObject.FindGameObjectWithTag("GM"));
+    }
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+    
+}
---
+++ Assets/Scripts/ButtonsRotation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotateSpeed = 180f;
+    private bool UpIsPressed;
+    private bool DownIsPressed;
+
+    public void TogglePressedUp(bool value)
+    {
+        UpIsPressed = value;
+    }
+    public void TogglePressedDown(bool value)
+    {
+        DownIsPressed = value;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (UpIsPressed)
+        {
+            OnClickLeft(); //or just move the rotation code here
+        }
+        if (DownIsPressed)
+        {
+            OnClickRight();
+        }
+    }
+
+    public void OnClickLeft()
+    {
+
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+
+    }
+    public void OnClickRight()
+    {
+
+        transform.Rotate(Vector3.forward * -rotateSpeed * Time.deltaTime);
+
+    }
+}
