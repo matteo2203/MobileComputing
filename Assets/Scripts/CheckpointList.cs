c24259769f68031c8396fa8f7434f39deea0e1b2
FTocci <federico.tocci99@libero.it>
2020-12-20 15:09:56 +0100

    scritti script per la gestione dei checkpoint

---
+++ Assets/Scripts/CheckpointList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointList : MonoBehaviour
+{
+    private SettingMaster sm;
+    public GameObject Checkpoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm= GameObject.FindGameObjectWithTag("SM").GetComponent<SettingMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sm.isEasy)
+        {
+            Checkpoints.SetActive(true);
+        }
+        else
+            Checkpoints.SetActive(false);
+    }
+}
