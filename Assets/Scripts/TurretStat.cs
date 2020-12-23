b31bd2e7801af467c85a852dc25059ed92d8dc46
Matteo1903 <marinimatteo99@gmail.com>
2020-12-23 10:39:42 +0100

    scritti script per la gestione delle statistiche delle torrette

---
+++ Assets/Scripts/TurretStat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TurretStat : MonoBehaviour
+{
+    public float delayTime;
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStat>().Esplosione();
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "PlayerBullet")
+            health--;
+    }
+}
