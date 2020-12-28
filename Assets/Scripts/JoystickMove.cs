94f7271c0099a00a76e7982ce55ad9f6334513f2
Matteo1903 <marinimatteo99@gmail.com>
2020-12-28 12:01:42 +0100

    scritti script per la gestione della levetta di movimento

---
+++ Assets/Scripts/JoystickMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickMove : MonoBehaviour
+{
+    public GameObject AudioMotore;
+    protected Joystick joystick;
+    private float _horizontalInput = 0;
+    private float _verticalInput = 0;
+    public int movementSpeed = 0;
+    public int rotationSpeed = 0;
+ 
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<FixedJoystick>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        AudioMotore.GetComponent<AudioSource>().mute = !(_horizontalInput != 0 || _verticalInput != 0);
+            GetPlayerInput();
+        
+    }
+
+    private void FixedUpdate()
+    {
+      
+        
+            RotatePlayer();
+            MovePlayer();
+        
+    }
+
+
+    private void GetPlayerInput()
+    {
+        _horizontalInput = joystick.Horizontal;
+        _verticalInput = joystick.Vertical;
+    }
+
+    private void RotatePlayer()
+    {
+        float rotation = -_verticalInput * rotationSpeed;
+        transform.Rotate(Vector3.forward * rotation);
+    }
+
+    private void MovePlayer()
+    {
+        if(_horizontalInput>0)
+            rb2d.velocity = transform.right * Mathf.Round(_horizontalInput) * movementSpeed;
+        else
+            rb2d.velocity = transform.right * Mathf.Round(_horizontalInput) * (movementSpeed/2);
+    }
+}
