76cc12055273298440606dcd3874a37d174f2b05
AressGia <giacomo.scordino@gmail.com>
2021-01-04 13:37:12 +0100

    Migliorata la gestione dei checkpoint

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster instance;
+    public int strCount;
+    public Transform lastCheckpointPos;
+    public List<string> collected = new List<string>();
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+            Destroy(gameObject);
+    }
+    void Destroy() 
+    {
+        Destroy(this);
+    }
+
+}
