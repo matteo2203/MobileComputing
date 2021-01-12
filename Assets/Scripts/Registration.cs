89cb480d3b9e1e51e43a9e9b3b3fa5d04f080bb5
matteomaraziti <m.maraziti@gmail.com>
2021-01-12 16:10:40 +0100

    Creati script per comunicare con il server per la gestione delle statistiche

---
+++ Assets/Scripts/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*public class Registration : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passField;
+    public Button submitButton;
+
+    public void CallRagister()
+    {
+       StartCoroutine(Register());
+    }
+    IEnumerator Register()
+    {
+        
+    }
+}*/
---
+++ Assets/Scripts/Servizi.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Servizi : MonoBehaviour
+{
+    public Boolean userExist;
+    public Boolean userRegistered;
+    public string nome;
+    public TMP_InputField nomFieldLog;
+    public TMP_InputField pswFieldLog;
+    public TMP_InputField nomFieldReg;
+    public TMP_InputField pswFieldReg;
+    void Start()
+    {
+
+    }
+
+    //Qui le funzioni di giacomino
+    
+    public void getUserFromServer()
+    {
+
+        HttpWebRequest request =
+            (HttpWebRequest)WebRequest.Create(
+                string.Format("https://mobilecomputingapp.herokuapp.com/getUser?nome={0}&password={1}", nomFieldLog.text, pswFieldLog.text));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonString = reader.ReadToEnd();
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Debug.Log(jsonString);
+            userExist = true;
+            this.nome = nomFieldLog.text;
+            this.gameObject.GetComponent<Menu>().Benvenuto();
+            GameObject.FindGameObjectWithTag("SM").GetComponent<SettingMaster>().nome = nome;
+        }
+        else
+        {
+            userExist = false;
+        }
+    }
+
+
+     public void registerUser()
+    {
+        HttpWebRequest request =
+               (HttpWebRequest)WebRequest.Create(
+                   string.Format("https://mobilecomputingapp.herokuapp.com/creaUser?nome={0}&password={1}", nomFieldReg.text, pswFieldReg.text));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonString = reader.ReadToEnd();
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Debug.Log(jsonString);
+            userRegistered = true;
+            this.nome = nomFieldReg.text;
+            this.gameObject.GetComponent<Menu>().Benvenuto();
+            GameObject.FindGameObjectWithTag("SM").GetComponent<SettingMaster>().nome = nome;
+        }
+        else
+        {
+            userRegistered = false;
+        }
+    }
+
+    public void getLivelli()
+    {
+
+    }
+
+    public void getScorePerUtente()
+    {
+
+    }
+}
+
+[Serializable]
+public class User
+{
+    public int id;
+    public string nome;
+    public string psw;
+}
