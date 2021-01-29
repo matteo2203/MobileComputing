2f06a89c38eeba6585d6b0cae1431a0ba679ffff
Giacomo Scordino <gia.scordino1@stud.uniroma3.it>
2021-01-30 00:16:53 +0100

    Aggiunti i servizi per gli score degli utenti

--- Servizi.js
+++ Servizi.js
@@ -61,3 +61,63 @@ app.get("/getUser", (req, res, next) => {
     }
   });
 });
+
+app.get("/getUserStatForLevel", (req, res, next) => {
+  var idUtente = req.query.utente;
+
+  con.query("SELECT * FROM punteggi WHERE id_utente = ?", [idUtente], (err, result) => {
+    if(err) throw err;
+    res.status(200).send({scores: result});
+  });
+});
+
+app.get("/saveUserStatForLevel", (req, res, next) => {
+  var idUtente = req.query.utente;
+  var livello = req.query.livello;
+  var score = req.query.score;
+  toUpdate = false;
+  con.query("SELECT score FROM punteggi WHERE id_utente = ? AND id_livello = ?", [idUtente, livello], (err, result) => {
+    if(err) throw err;
+    if(result.score < score) toUpdate = true;
+  });
+
+  if(!toUpdate) {
+    con.query("INSERT INTO punteggi VALUES (?, ?, ?)", [livello, idUtente, score], (err, result) => {
+      if(err) throw err;
+      res.status(200).send({status: "ok"});
+    });
+  } else {
+    con.query("UPDATE punteggi SET score = ? WHERE id_utente = ? AND id_livello = ?", [score, idUtente, livello], (err, result) => {
+      if(err) throw err;
+      res.status(200).send({status: "ok"});
+    });
+  }
+});
+
+app.get("/getRankForLevels", (req, res, next) => {
+  var scores = {};
+
+  con.query("SELECT nome, score FROM punteggi JOIN utenti ON id_utente = id AND id_livello = 1 ORDER BY score DESC LIMIT 10", (err, result) => {
+    if(err) throw err;
+    scores.lvl1 = result;
+  });
+  con.query("SELECT nome, score FROM punteggi JOIN utenti ON id_utente = id AND id_livello = 2 ORDER BY score DESC LIMIT 10", (err, result) => {
+    if(err) throw err;
+    scores.lvl2 = result;
+  });
+  con.query("SELECT nome, score FROM punteggi JOIN utenti ON id_utente = id AND id_livello = 3 ORDER BY score DESC LIMIT 10", (err, result) => {
+    if(err) throw err;
+    scores.lvl3 = result;
+    res.status(200).send(scores);
+  });
+});
+
+app.get("/getGlobalScores", (req, res, next) => {
+  con.query("SELECT nome, scoreTotale as score FROM SommaLivelliPerUtente ORDER BY scoreTotale DESC LIMIT 10", (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.status(200).send({
+      scores: result
+    });
+  });
+});
